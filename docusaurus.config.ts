182494807fd8c2ec2f4151370fbc0aa2128787ff
AndrewNation <andrewpereira.megabyte@gmail.com>
2025-05-29 19:01:28 +0000

    arusian academy theming

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -82,7 +82,7 @@ const config: Config = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://conworkshop.com/view_language.php?l=ARJ',
           label: 'Conworkshop',
           position: 'right',
         },
