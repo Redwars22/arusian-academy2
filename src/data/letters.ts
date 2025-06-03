17148095bb34bc139bb40dfe0308abec22b7447d
AndrewNation <andrewpereira.megabyte@gmail.com>
2025-06-03 16:27:13 +0000

    semlek game draft

---
+++ src/data/letters.ts
@@ -0,0 +1,272 @@
+export const letters = {
+    vowels: [
+        {
+            translit: "ei",
+            arukey: "A",
+            ipa: "ei"
+        },
+        {
+            translit: "e",
+            arukey: "e",
+            ipa: "ɛ"
+        },
+        {
+            translit: "i",
+            arukey: "i",
+            ipa: "i"
+        },
+        {
+            translit: "a",
+            arukey: "a",
+            ipa: "a"
+        },
+        {
+            translit: "r",
+            arukey: "r",
+            ipa: "ɜ ~ ɜʁ"
+        },
+        {
+            translit: "u",
+            arukey: "u",
+            ipa: "u"
+        },
+        {
+            translit: "o",
+            arukey: "o",
+            ipa: "o"
+        }
+    ] as {
+        translit: string;
+        arukey: string;
+        ipa: string;
+    }[],
+    consonants: [
+        {
+            translit: "b",
+            arukey: "b",
+            ipa: "b"
+        },
+        {
+            translit: "k",
+            arukey: "k",
+            ipa: "k"
+        },
+        {
+            translit: "sh",
+            arukey: ",k",
+            ipa: "ʃ"
+        },
+        {
+            translit: "d",
+            arukey: "d",
+            ipa: "d"
+        },
+        {
+            translit: "t",
+            arukey: "-d",
+            ipa: "t"
+        },
+        {
+            translit: "f",
+            arukey: "f",
+            ipa: "f"
+        },
+        {
+            translit: "g",
+            arukey: "g",
+            ipa: "g"
+        },
+        {
+            translit: "sh",
+            arukey: "J",
+            ipa: "ʃ"
+        },
+        {
+            translit: "z",
+            arukey: "-j",
+            ipa: "s],[z"
+        },
+        {
+            translit: "l",
+            arukey: "l",
+            ipa: "l"
+        },
+        {
+            translit: "m",
+            arukey: "m",
+            ipa: "m"
+        },
+        {
+            translit: "n",
+            arukey: "n",
+            ipa: "n"
+        },
+        {
+            translit: "p",
+            arukey: "P",
+            ipa: "p"
+        },
+        {
+            translit: "b",
+            arukey: "-p",
+            ipa: "b"
+        },
+        {
+            translit: "s",
+            arukey: "s",
+            ipa: "s"
+        },
+        {
+            translit: "z",
+            arukey: ",s",
+            ipa: "z"
+        },
+        {
+            translit: "t",
+            arukey: "t",
+            ipa: "t"
+        },
+        {
+            translit: "v",
+            arukey: "v",
+            ipa: "v"
+        },
+        {
+            translit: "gz",
+            arukey: "x",
+            ipa: "g],[gz"
+        },
+        {
+            translit: "r",
+            arukey: "r",
+            ipa: "ʁ"
+        },
+        {
+            translit: "j",
+            arukey: "j",
+            ipa: "ʒ"
+        }
+        /*{
+            translit: "",
+            arukey: "k",
+            ipa: "k"
+        },
+        {
+            translit: "",
+            arukey: ",k",
+            ipa: "ʃ"
+        },
+        {
+            translit: "d",
+            arukey: "d",
+            ipa: "d"
+        },
+        {
+            translit: "",
+            arukey: "-d",
+            ipa: "t"
+        },
+        {
+            translit: "f",
+            arukey: "f",
+            ipa: "f"
+        }, {
+            translit: "g",
+            arukey: "g",
+            ipa: "g"
+        }, {
+            translit: "j",
+            arukey: "j",
+            ipa: "ʒ"
+        }, {
+            translit: "k",
+            arukey: "k",
+            ipa: "k"
+        }, 
+        {
+            translit: "",
+            arukey: "z",
+            ipa: "z"
+        },
+        {
+            translit: "",
+            arukey: ",c",
+            ipa: "ʃ"
+        },
+        {
+            translit: "l",
+            arukey: "l",
+            ipa: "l"
+        }, {
+            translit: "m",
+            arukey: "m",
+            ipa: "m"
+        }, {
+            translit: "n",
+            arukey: "n",
+            ipa: "n"
+        }, {
+            translit: "p",
+            arukey: "p",
+            ipa: "p"
+        }, 
+        {
+            translit: "",
+            arukey: "-b",
+            ipa: "b"
+        },
+        {
+            translit: "r",
+            arukey: "r",
+            ipa: "ʁ"
+        }, {
+            translit: "s",
+            arukey: "s",
+            ipa: "s"
+        }, 
+        {
+            translit: "",
+            arukey: "z",
+            ipa: "z"
+        },
+        {
+            translit: "t",
+            arukey: "t",
+            ipa: "t"
+        },
+        {
+            translit: "",
+            arukey: "x",
+            ipa: "gz ~ g"
+        },
+        {
+            translit: "",
+            arukey: ",r",
+            ipa: "ɛʁ"
+        },
+        {
+            translit: "",
+            arukey: ",r",
+            ipa: "ɛʁ"
+        },
+        {
+            translit: "",
+            arukey: ",r",
+            ipa: "ɛʁ"
+        },
+        {
+            translit: "",
+            arukey: ",r",
+            ipa: "ɛʁ"
+        },
+        {
+            translit: "",
+            arukey: ",r",
+            ipa: "ɛʁ"
+        }*/
+    ] as {
+        translit: string;
+        arukey: string;
+        ipa: string;
+    }[]
+}
